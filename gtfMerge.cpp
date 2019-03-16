220b0c0230676ef78b56084fe4d2de3935819c54
Matthew Patrick <mattimpat@gmail.com>
2019-03-15 21:07:09 -0400

    Update gtfMerge.cpp

--- gtfMerge.cpp
+++ gtfMerge.cpp
@@ -1,102 +1,80 @@
 #include <gtf.h>
-#include <iostream>
-#include <boost/program_options.hpp>
-#include <boost/filesystem/operations.hpp>
-#include <boost/filesystem/path.hpp>
 
 namespace gtf{
 
-   bool isOverlapping(std::vector<transfrag> &tf1, std::vector<transfrag> &tf2, int minBasePairs=50, double minPercentage=20.0){
-      int count=0, fragmentSize=0; auto tf2Iter=tf2.begin();
-      for(auto tf1Iter=tf1.begin(); tf1Iter!=tf1.end(); ++tf1Iter){
-         fragmentSize+=tf1Iter->end-tf1Iter->start;
-         while(tf2Iter!=tf2.end()){
+   bool isOverlapping(Transcript &tf1, Transcript &tf2, int minBasePairs=50, double minPercentage=20.0){
+      int count=0, fragmentSize=0; auto tf2Iter=tf2.fragments.begin();
+      for(auto tf1Iter=tf1.fragments.begin(); tf1Iter!=tf1.fragments.end(); ++tf1Iter){
+         fragmentSize+=tf1Iter->end-tf1Iter->start+1;
+         while(tf2Iter!=tf2.fragments.end()){ //find nearest tf2 to tf1
             if(tf2Iter->end > tf1Iter->start){break;}
             tf2Iter++;
          }
-         while(tf2Iter!=tf2.end()){
+         while(tf2Iter!=tf2.fragments.end()){
             int c=std::min(tf2Iter->end,tf1Iter->end)-std::max(tf2Iter->start,tf1Iter->start)+1;
-            if(c > 0){count+=c;} else{break;}
+            if(c > 0){count+=c;} else{break;} //positive values of c indicate some overlap
             tf2Iter++;
          }
-         if(tf2Iter==tf2.end()){break;} else{tf2Iter--;}
+         if(tf2Iter==tf2.fragments.end()) break; //no more tf2s to overlap with tf1s
       }
       if(count>=minBasePairs || count>=minPercentage*fragmentSize){return true;} else {return false;}
    }
 
    void merge(TranscriptMap &ref, TranscriptMap &target, TranscriptMap &add, int minBasePairs, double minPercentage){
       for(auto targetIter=target.begin(); targetIter!=target.end();){
-         bool overlap=false;
+         bool overlap=false; //loop through all transcripts each time, as TranscriptMap is unordered
          for(auto refIter=ref.begin(); refIter!=ref.end(); ++refIter){
-            if(targetIter->second.begin()->seqname == refIter->second.begin()->seqname)
-               if(targetIter->second.begin()->strand==refIter->second.begin()->strand || targetIter->second.begin()->strand=="." || refIter->second.begin()->strand==".")
+            if(targetIter->second.seqname == refIter->second.seqname)
+               if(targetIter->second.strand==refIter->second.strand || targetIter->second.strand=="." || refIter->second.strand==".") //transcripts containing only one fragment may be in either order
                   if(isOverlapping(targetIter->second,refIter->second,minBasePairs,minPercentage)){overlap=true; break;}
          }
          if(!overlap){
             add[targetIter->first]=targetIter->second;
-            targetIter=target.erase(targetIter);
+            targetIter=target.erase(targetIter); //points to next element
          }
          else
             targetIter++;
       }
       for (auto addIter=add.begin(); addIter!=add.end(); ++addIter){
-         ref[addIter->first]=addIter->second;
+         ref[addIter->first]=addIter->second; //important to add at the end, to avoid interfering with loop
       }
    }
 }
 
 int main(int argc, const char* argv[])
 {
-   namespace po = boost::program_options;
-   po::options_description desc("Command line options");
-   desc.add_options()
-      ("help","produce help message")
-      ("outDir",po::value<std::string>(),"set output directory (default: \"./\")")
-      ("outPre",po::value<std::string>(),"set output prefix (default: \"\")")
-      ("minBP",po::value<int>(),"set minimum number basepairs for overlap (default: 50)")
-      ("minPct",po::value<double>(),"set minimum percentage for overlap (default: 20.0)")
-      ("refGTF",po::value<std::string>(),"set reference file (can be specified without the flag)")
-      ("targetGTF",po::value<std::string>(),"set target file (can be specified without the flag)")
-   ;
+   try{
+      int minBasePairs;
+      double minPercentage;
+      gtf::programOptions opt("gtfmerge v0.1 (9 March 2019)",true,2);
+      opt.addOption(minBasePairs,"minBP","Minimum number of basepairs for overlap",true,50);
+      opt.addOption(minPercentage,"minPercent","Minimum percentage for overlap)",true,20.0);
+      opt.parse(argc,argv);
 
-   po::positional_options_description p;
-   p.add("refGTF",1).add("targetGTF",1);
+      if(opt.file_names.size()<2){
+         BOOST_LOG_TRIVIAL(error) << "Error: gtfmerge requires at least 2 GTF files to merge";
+         return(1);
+      }
 
-   po::variables_map vm;
-   po::store(po::command_line_parser(argc,argv).options(desc).positional(p).run(),vm);
-   po::notify(vm);
-
-   if(vm.count("help")){
-      std::cout<<desc<<std::endl;
-      exit(0);
+      BOOST_LOG_TRIVIAL(info)<<"Reading GTF 0...";
+      gtf::gtf g1(opt.file_names[0],"_0");  //first reference file
+      for(int addIdx=1; addIdx<opt.file_names.size(); ++addIdx){
+         BOOST_LOG_TRIVIAL(info)<<"Reading GTF "<<addIdx<<"...";
+         gtf::gtf g2(opt.file_names[addIdx],"_"+std::to_string(addIdx));
+         gtf::gtf g3; //temporary GTF to store added transcripts
+         BOOST_LOG_TRIVIAL(info)<<"Merging GTF "<<addIdx-1<<" and "<<addIdx<<"...";
+         gtf::merge(g1.transcripts, //reference GTF from first round becomes reference for next
+                    g2.transcripts, //target GTF from first round becomes temporary GTF for overlap
+                    g3.transcripts,
+                    minBasePairs,minPercentage);
+         BOOST_LOG_TRIVIAL(info)<<"Writing overlapping and added transcripts from GTF "<<addIdx<<"...\n";
+         g2.writeToFile(opt.outPrefix+"_overlap"+std::to_string(addIdx)+".gtf");
+         g3.writeToFile(opt.outPrefix+"_added"+std::to_string(addIdx)+".gtf");
+      }
+      BOOST_LOG_TRIVIAL(info)<<"Writing output...\n";
+      g1.writeToFile(opt.outPrefix+"_combined.gtf");
    }
-
-   std::string outDir=".",outPrefix="",ref="",target="";
-   int minBasePairs=50; double minPercentage=20.0;
-   if(vm.count("outDir")) outDir=vm["outDir"].as<std::string>();
-   if(vm.count("outPre")) outPrefix=vm["outPre"].as<std::string>();
-   if(vm.count("minBP")) minBasePairs=vm["minBP"].as<int>();
-   if(vm.count("minPct")) minPercentage=vm["minPct"].as<int>();
-   if(vm.count("refGTF")) ref=vm["refGTF"].as<std::string>();
-   else{std::cerr<<"Error: reference file must be specified"<<std::endl; exit(EXIT_FAILURE);}
-   if(vm.count("targetGTF")) target=vm["targetGTF"].as<std::string>();
-   else{std::cerr<<"Error: target file must be specified"<<std::endl; exit(EXIT_FAILURE);}
-
-   boost::filesystem::path outPath(outDir);
-   if(!boost::filesystem::is_directory(outDir)){
-      std::cout<<"Warning:: creating output directory ("<<outDir<<") as it does not currently exist"<<std::endl;
-      boost::filesystem::create_directory(outDir);
+   catch(const std::exception &e){
+      BOOST_LOG_TRIVIAL(error)<<e.what();
    }
-
-   std::cout<<"Reading target..."<<std::endl;
-   gtf::gtf<'1'> g2(target);
-   std::cout<<"Reading reference..."<<std::endl;
-   gtf::gtf<'0'> g1(ref); gtf::gtf<'_'> g3;
-   std::cout<<"Merging target and reference..."<<std::endl;
-
-   gtf::merge(g1.transcripts,g2.transcripts,g3.transcripts,minBasePairs,minPercentage);
-   std::cout<<"Writing output..."<<std::endl;
-   g1.writeToFile(outDir+"/"+outPrefix+"combined.gtf");
-   g2.writeToFile(outDir+"/"+outPrefix+"overlap.gtf");
-   g3.writeToFile(outDir+"/"+outPrefix+"added.gtf");
-}            
+}
